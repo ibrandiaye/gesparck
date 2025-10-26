7996da669c385ba51c31612f935c0ce77c28c828
ibrandiaye <ibra789ndiaye@gmail.com>
2025-10-26 19:38:35 +0000

    dddd

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -37,6 +37,7 @@ class TripController extends Controller
 
         $trips = $query->orderBy('date_trajet', 'desc')
                       ->orderBy('created_at', 'desc')
+                      ->orderBy('vehicle_id', 'desc')
                       ->paginate(20);
 
         $vehicles = Vehicle::all();
