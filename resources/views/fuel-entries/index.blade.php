906e41c27632af22d93bf21d092dc2f6c9501b1f
ibrandiaye <ibra789ndiaye@gmail.com>
2025-11-19 16:35:16 +0000

    dddd

--- resources/views/fuel-entries/index.blade.php
+++ resources/views/fuel-entries/index.blade.php
@@ -288,7 +288,7 @@
             url: 'https://cdn.datatables.net/plug-ins/2.3.4/i18n/fr-FR.json',
         },
         ordering:true,
-        order: [[0, 'desc']],
+        order: [[0, 'asc']],
          lengthMenu: [10, 25, 50, 100, 200,500,1000]
     });
 </script>
