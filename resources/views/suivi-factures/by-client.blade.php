69ce3d39ea06d298cc04ff263bcef4fc517aa265
ibrandiaye <ibra789ndiaye@gmail.com>
2025-11-19 17:22:48 +0000

    dddd112345

--- resources/views/suivi-factures/by-client.blade.php
+++ resources/views/suivi-factures/by-client.blade.php
@@ -41,7 +41,7 @@
                                     </div>
                                     <div class="col-md-3">
                                         <strong>Montant total:</strong><br>
-                                        <span class=" text-danger">
+                                        <span class=" badge bg-danger">
                                             <h4>{{ number_format($factures->sum('montant'), 0, ',', ' ') }} CFA </h4>
                                         </span>
                                     </div>
